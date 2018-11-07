b6800c0a3a24ec06af0d162ba1fd3e1e6bdb165a
knwch <knwch@knwchs-MacBook-Pro.local>
2018-11-07 13:20:00 +0700

    Initial commit

---
+++ src/Pad.java
@@ -0,0 +1,75 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.*;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Pad
+{
+    private JPanel mainPanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+
+    public Pad() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                File Sound = new File("profile", "L_KICK_037.wav");
+                playSound(Sound);
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                File Sound = new File("profile", "L_KICK_038.wav");
+                playSound(Sound);
+            }
+        });
+
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                File Sound = new File("profile", "L_SNARE_011.wav");
+                playSound(Sound);
+            }
+        });
+
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                File Sound = new File("profile", "L_SNARE_013.wav");
+                playSound(Sound);
+            }
+        });
+    }
+
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Pad");
+        frame.setContentPane(new Pad().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    static void playSound(File Sound)
+    {
+        try
+        {
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(Sound));
+            clip.start();
+
+            Thread.sleep(clip.getMicrosecondLength()/100000);
+        }
+        catch(Exception e)
+        {
+
+        }
+    }
+
+}
