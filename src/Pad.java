717e331e57903b4cff6cc8bca784ccde09b83c9a
TIME <famfew25@gmail.com>
2018-11-10 02:38:27 +0700

    Changed

--- src/Pad.java
+++ src/Pad.java
@@ -108,9 +108,11 @@ public class Pad
     public static void main(String[] args)
     {
         JFrame frame = new JFrame("Pad");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(new Pad().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
