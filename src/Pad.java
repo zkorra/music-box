a63b5df95629ce203b48446880cbf2a9c5ef5676
knwch <t.korrawich@protonmail.com>
2018-11-09 21:29:06 +0700

    ...

--- src/Pad.java
+++ src/Pad.java
@@ -13,6 +13,7 @@ public class Pad
     private JButton button3;
     private JButton button4;
 
+
     public Pad() {
         button1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
