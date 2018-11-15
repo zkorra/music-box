3a60186ef79fc673082b116aff22b7f8bf1e9c35
knwch <t.korrawich@protonmail.com>
2018-11-15 17:39:24 +0700

    ...

--- src/Music_b/Sound.java
+++ src/Music_b/Sound.java
@@ -5,21 +5,32 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.io.File;
 
-public class Sound {
-
-    public static void playSound(File fileName)
+public class Sound
+{
+    public static void play(File fileName)
     {
-        new Thread(new Runnable() {
-            public void run() {
-                try {
+        new Thread(new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
                     Clip clip = AudioSystem.getClip();
                     AudioInputStream inputStream = AudioSystem.getAudioInputStream(fileName);
                     clip.open(inputStream);
                     clip.start();
-                } catch (Exception e) {
-                    System.out.println("play sound error: " + e.getMessage() + " for " + fileName);
+                }
+                catch (Exception e)
+                {
+                    System.out.println("error: " + e.getMessage() + " for " + fileName);
                 }
             }
         }).start();
     }
+
+    public static void stop()
+    {
+
+    }
+
 }
